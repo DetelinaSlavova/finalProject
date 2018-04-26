55d064b3268be5d43b99baa6eb42a0fcecba1a43
Vasil Evgeniev Aleksandrov <31408375+vaseto27@users.noreply.github.com>
2018-04-26 21:16:59 +0300

    Add files via upload

--- tehnopoli/routes/product.js
+++ tehnopoli/routes/product.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
-router.get('/', function(req, res,){
-    var products = req.db.get('products');
-    products.find({},{sortBy: 'price'}, function(product){
-        res.json(products);
-
+router.get('/', function(req, res){
+    var db = req.db;
+    var collection = req.db.get('TvVideoGaming');
+   
+    collection.find({}, {}, function(err, result){
+        if(err){
+            res.status(500);
+            res.json({err})
+        }else{
+            res.status(200);
+            res.json(result);
+        }
     });
 });
 
