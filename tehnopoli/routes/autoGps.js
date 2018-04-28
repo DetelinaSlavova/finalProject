11d187e41f370c41657f3e9264a7d55202aa7ec7
Vasil Evgeniev Aleksandrov <31408375+vaseto27@users.noreply.github.com>
2018-04-29 00:51:00 +0300

    Add files via upload

---
+++ tehnopoli/routes/autoGps.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+router.get('/', function(req, res){
+    var db = req.db;
+    var collection = req.db.get('AutoGps');
+
+    collection.find({}, {}, function(err, result){
+        if(err){
+            res.status(500);
+            res.json({err})
+        }else{
+            res.status(200);
+            res.json(result);
+        }
+    });
+});
+
+module.exports = router;    
