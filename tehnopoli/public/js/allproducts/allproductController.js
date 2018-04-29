b5897338463fdca3f1d543c6d0195080779ce7cb
Vasil Evgeniev Aleksandrov <31408375+vaseto27@users.noreply.github.com>
2018-04-30 00:43:22 +0300

    Add files via upload

--- tehnopoli/public/js/allproducts/allproductController.js
+++ tehnopoli/public/js/allproducts/allproductController.js
@@ -1,18 +1,30 @@
 app.controller('allproductController',function ($scope, allproductService) {
     allproductService.getAllProducts().then(function(allProducts){
         $scope.allProducts = allProducts.data;
-        // $scope.complete = function(string){
-        //     var output = [];
-        //     angular.forEach($scope.allProducts,function(product){
-        //         if(product.toLowerCase().indexOf(string.toLowerCase()) >= 0){
-        //             output.push(product);
-        //         }
-        //     });
-        //     $scope.filterProduct = output;
-        // }
+        $scope.names = [];
+
+        angular.forEach($scope.allProducts,function(p){
+            $scope.names.push(p.name);
+        });
+        console.log($scope.names);
+        $scope.complete = function(string){
+            var output = [];
+            angular.forEach($scope.names, function(product){
+                if(product.toLowerCase().indexOf(string.toLowerCase()) >= 0){
+                    output.push(product);
+                }
+            });
+            $scope.filterProduct = output;
+            console.log($scope.filterProduct);
+
+        }
         $scope.fillTextbox = function(string){
             $scope.product = string;
             $scope.hidethis = true;
         }
+        
+        // angular.forEach($scope.allProducts,function(p){
+        //     console.log(p.name + ' ' + p._id);
+        // }) 
     });
 });
