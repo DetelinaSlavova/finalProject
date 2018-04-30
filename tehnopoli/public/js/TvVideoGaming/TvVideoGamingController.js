1eb26fa8987d1a254303e431bfdc37ef263c0559
DetelinaSlavova <lina_dimitrova@abv.bg>
2018-04-30 14:32:49 +0300

    css product

--- tehnopoli/public/js/TvVideoGaming/TvVideoGamingController.js
+++ tehnopoli/public/js/TvVideoGaming/TvVideoGamingController.js
@@ -1,10 +1,16 @@
 app.controller('TvVideoGamingController', function ($scope, TvVideoGamingService) {
-    TvVideoGamingService.getTvs().then(function(tvs){
-        $scope.tvs = tvs.data;
-        $scope.tvs.forEach(function(i){
-            $scope.info =(i.info[0]);
-            // console.log($scope.info)
-            
-       }) 
+    TvVideoGamingService.getTvs().then(function(allProducts){
+        $scope.allProducts = allProducts.data;
+       
+     
+            $scope.products = $scope.allProducts.filter(function(i){
+                if(i.type ==="tv") {
+                   return $scope.allProducts;
+                } 
+            })
+      
+       
+      
+
     });
 })
