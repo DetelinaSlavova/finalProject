eea1798676623a96222a6d38b3c94e6bf6e4b16d
swetlasg <svetulka_24@mail.bg>
2018-05-03 19:21:14 +0300

    again

--- tehnopoli/public/js/login/loginController.js
+++ tehnopoli/public/js/login/loginController.js
@@ -1,7 +1,83 @@
-app.controller('loginController', function ($scope, loginService, $location) {
+app.controller('loginController', function ($scope,$http, $rootScope, loginService, $location) {
+    //login
+    $scope.wrongPassword = false;
+    $scope.wrongEmail = false;
+    $scope.noUser = false;
+    $scope.loggedUserMail = "";
+   
 
-    $scope.changeLocation = function (path){
-        $location.path(path);
+    $scope.logedUser = {
+        email: '',
+        password: '',
+    }
+
+    $scope.isError = false;
+    
+
+    const PASS_LENGHT = 8;
+    const MAX_LENGHT = 25;
+ 
+    function isValidEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
     };
-  
+   
+   function isValidString(str) {
+     return (typeof str === 'string' && str.length > 0 && str.length <=MAX_LENGHT);
+   };
+   
+   
+   function isValidPassword(pass) {
+     return (isValidString && pass.length >= PASS_LENGHT && pass.length<=MAX_LENGHT)
+   };
+
+   function changeLocation (path){
+    $location.path(path);
+ }
+    angular.element('#loginEmail').on('click', function () {
+        
+            $scope.wrongEmail = false;
+            $scope.noUser = false;
+        
+    });
+
+    angular.element('#loginPassword').on('focus', function () {
+            $scope.wrongPassword = false;
+            $scope.noUser = false;
+            $scope.isError = false;
+        
+    });
+
+    $scope.userLogin = function ($event, path) {
+        $event.preventDefault();
+        
+
+        if (!isValidEmail($scope.logedUser.email)) {
+            $scope.wrongEmail = true;
+            return;
+        }
+        if (!isValidPassword($scope.logedUser.password)) {
+            $scope.wrongPassword = true;
+            return;
+        }
+
+        loginService.login($scope.logedUser)
+            .then(function (response) {
+                $scope.isLoged = false;
+                if (response.status >= 200 && response.status <= 399) {
+                    $rootScope.logedUser = response.data;
+                    alert ("Вие влязохте успешно" + response.data)
+                    console.log(response.data)
+                    $scope.loggedUserMail = response.data.isLogged
+
+                    $location.path('/');
+                  }
+                })
+                  .catch(function (err) {
+                    alert("Грешно име или парола");
+                  });
+            
+              } 
+              
+              
 })
