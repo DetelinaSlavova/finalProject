61e017c1a36e9b1314ddda4a831ae074a3c63ad7
DetelinaSlavova <lina_dimitrova@abv.bg>
2018-05-04 14:26:00 +0300

    deleted TVVideoGaming

--- TvVideoGaming/TvVideoGamingService.js
+++
@@ -1,8 +0,0 @@
-app.service('TvVideoGamingService',function($http){
-    this.getTvs = (type) => $http.get('http://localhost:3000/TvVideoGaming?type=' + type);
-    this.cartProduct = function(){ 
-    var productForBuy = [];
-    console.log(productForBuy);
-    return productForBuy
-    }
-});
